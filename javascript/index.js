e5c2fa81041eadb5330473e915fa18c17fa50855
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-20 18:43:52 -0500

    Add variables section and arithmetic, comparison, and logical sections

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,41 @@ let integer = 0;
 let float = 10.5;
 let string = "hello"
 let boolean = true;
+
+
+// variables
+
+
+let variable = "I can change";
+
+const constant = "I cannot change";
+
+
+// operators
+
+
+let a = 10;
+let b = 20;
+
+
+// arithmetic
+
+let sum = a + b;
+let diff = a - b;
+let prod = a * b;
+let quot = a / b;
+
+// comparison 
+
+let isEqual = ( a === b) // strict equality
+let isEqual2 = ( a == b) // equality
+
+let isNotEqual = ( a !== b) // strict inequality
+let isNotEqual2 = ( a != b) // inequality
+
+// logical
+
+let and = (a < b && b > 10); // AND
+
+let or = (a < b || b < 10); // OR
+
