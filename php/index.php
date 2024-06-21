5b65905d34e7e6ceb046d38539debc63c78b3400
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-21 12:27:09 -0500

    Add variables and operators sections for PHP

--- php/index.php
+++ php/index.php
@@ -5,6 +5,48 @@ $floatNum = 10.5; // float
 $string = "Hello"; // string
 $boolean = true; // boolean
 // notice how PHP REQUIRES semicolons, do not forget them!
+// also remember to wrap php code in <?php tags !
+
+
+// variables
+
+$variable = 10;
+// declare variables using the $
+
+
+define("CONSTANT", 20); // constants are defined with the define function
+
+
+
+
+// operators
+
+
+
+$a = 10;
+$b = 20;
+
+
+// arithmetic
+
+$sum = $a + $b; // add
+$diff = $a - $b; // sub
+$prod = $a * $b; // mult
+$quot = $a / $b; // divide
+
+
+// comparison
+
+$isEqual = ($a == $b); // equality
+$isNotEqual = ($a != $b); // inequality
+$isGreater = ($a > $b); // greater than
+$isLess = ($a < $b); // less than
+
+
+// logical
+$andOp = ($a < $b && $b > 10); // AND 
+$orOp = ($a < $b || $b < 10); // OR
+
 
 
 ?>
