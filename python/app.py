37fe2aa1c3a9bd08a51aacb02565a7d1657bbb48
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-21 11:21:59 -0500

    Add variables and operators section

--- python/app.py
+++ python/app.py
@@ -6,3 +6,37 @@ string = "Hello" #string
 boolean = True #Boolean
 
 
+
+#variables 
+
+variable = 10
+CONSTANT = 20 # constants are usually defined with uppercase variables
+
+
+#operators
+
+a = 10
+b = 20
+
+
+# arithmetic
+
+sum_result = a + b # addition
+diff = a - b # subtraction
+prod = a * b # mult
+quot = a / b # division
+
+# comparison 
+is_equal = (a == b) # equality
+is_not_equal = (a != b) #inequality
+is_greater = (a > b) #greater than
+is_less_than = (a < b) #less than
+
+
+# logical
+
+and_op = (a < b and b > 10) #AND notice how the operator is the word and instead of the &&
+
+or_op = (a < b or b < 10) #OR notice how the operator is the word or instead of the ||
+
+
