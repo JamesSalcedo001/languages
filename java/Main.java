67ab026143513cf4a26e2dabe189e8d7105e5a26
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-20 19:54:49 -0500

    Create Java file, add data types, variables, and operators sections

---
+++ java/Main.java
@@ -0,0 +1,53 @@
+// data types
+
+int integer = 10; // integer
+
+float floatNum = 10.5f; // float
+
+String string = "Hello"; // string
+
+boolean bool = true; // boolean
+
+
+
+
+
+// variables 
+
+int variable = 10;
+
+final int CONSTANT = 20; // constants are defined with the final keyword
+
+
+
+
+
+
+// operators 
+
+int a = 10;
+int b = 20;
+
+// arithmetic
+
+int sum = a + b; // addition
+int diff = a - b; // subtraction
+int prod = a * b; // mult
+float quot = (float) a / b; // division type-casting to float
+
+// comparison operators
+
+boolean isEqual = (a == b); // equality
+boolean isNotEqual = (a != b); // inequality
+boolean isGreater = (a > b); // greater than
+boolean isLessThan = (a < b); // less than
+
+
+// logical ops
+
+boolean andOp = (a < b && b > 10); // AND
+boolean orOp = (a < b || b > 10); // OR
+
+
+
+
