30623783a284b0bf826c74619945c600f24dfda6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-21 12:16:37 -0500

    Create index.php file, add data types section in PHP

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$integer = 10; // integer 
+$floatNum = 10.5; // float
+$string = "Hello"; // string
+$boolean = true; // boolean
+// notice how PHP REQUIRES semicolons, do not forget them!
+
+
+?>
