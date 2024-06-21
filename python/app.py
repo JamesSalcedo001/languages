a4db92c18c3fa5618e7d158e7686459650841210
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-21 11:29:13 -0500

    Add control flow and loops sections for Python

--- python/app.py
+++ python/app.py
@@ -40,3 +40,49 @@ and_op = (a < b and b > 10) #AND notice how the operator is the word and instead
 or_op = (a < b or b < 10) #OR notice how the operator is the word or instead of the ||
 
 
+
+
+# control flow
+
+number = 10
+
+# if statement
+if number > 5:
+    print("Greater than 5")
+else:
+    print("5 or less")
+
+# notice the usage of the colon instead of the usual curly braces and parentheses you are used to
+
+
+
+
+
+# loops 
+
+
+# for loop
+for i in range(5):
+    print(i)
+
+# notice how this is entirely different syntax from other languages like javascript etc
+
+
+# while loop
+j = 0
+while j < 5:
+    print(j)
+    j += 1
+
+
+# do while loop
+# python does not have this, but can simulate it with a while loop
+
+k = 0
+while True:
+    print(k)
+    k += 1
+    if k >= 5:
+        break
+
+
