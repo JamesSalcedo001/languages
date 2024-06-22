3979152dcfc380e666d8188543e00baa073001e0
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-22 12:55:36 -0500

    Add polymorphism section for PHP

--- php/index.php
+++ php/index.php
@@ -262,4 +262,51 @@ echo $emp->getSalary(); //output: 50000
 
 
 
+
+
+// polymorphism (OOP)
+
+
+
+abstract class Shape {
+    abstract public function area();
+}
+
+
+class Circle extends Shape {
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function area() {
+        return pi() * $this->radius * $this->radius;
+    }
+}
+
+
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+
+
+$shapes = [new Circle(10), new Rectangle(5, 10)];
+
+foreach ($shapes as $shape) {
+    echo $shape->area() . "\n";
+}
+
+
 ?>
