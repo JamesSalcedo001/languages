3f187e7caf31cf0d1baeb610d8d2bfc9d294dc21
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-22 14:29:24 -0500

    Add operators section to TS

--- typescript/index.ts
+++ typescript/index.ts
@@ -17,3 +17,27 @@ const CONSTANT: number = 20; // constants defined by const keyword, and conventi
 
 
 
+// operators
+
+
+
+let a: number = 10;
+let b: number = 20;
+
+
+// arithmetic
+let sum: number = a + b; //add
+let diff: number = a - b; //subt
+let prod: number = a * b; // mult
+let quot: number = a / b; // divide
+
+
+// comparison
+let isEqual: boolean = (a === b); // strict equality
+let isNotEqual: boolean = (a !== b); // strict inequality
+let isGreater: boolean = (a > b); // greater than
+
+
+// logical
+let andOp: boolean = (a < b && b > 10); // AND
+let orOp: boolean = (a < b || b < 10); // OR
