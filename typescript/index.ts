1a96834fb3017070f08fb133e4b2300d8da8b210
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-22 14:20:11 -0500

    Add typescript file and data types section

---
+++ typescript/index.ts
@@ -0,0 +1,10 @@
+// data types
+
+
+let integer:  number = 10; //integer
+let floatNum: number = 10.5 // float
+let string: string = "Hello"; // string
+let boolean: boolean = true;  //boolean
+
+
+
