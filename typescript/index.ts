689410219da1e90a4ad0c831439ed6eb4857526a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-22 14:24:40 -0500

    Add variable section to TS

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,9 +2,18 @@
 
 
 let integer:  number = 10; //integer
-let floatNum: number = 10.5 // float
+let floatNum: number = 10.5 // float -> optional semicolons like js
 let string: string = "Hello"; // string
 let boolean: boolean = true;  //boolean
+
+// right off the bat, syntax is definitely different
+
+
+// variables
+
+
+let variable: number = 10;
+const CONSTANT: number = 20; // constants defined by const keyword, and convention is to type it in all caps
 
 
 
