c5cd3406e01078b85e4cb1e63d8d283bdbf9d44b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-23 14:17:42 -0500

    Add operators section

--- ruby/main.rb
+++ ruby/main.rb
@@ -13,3 +13,31 @@ boolean = true #boolean
 variable = "I can change"
 
 CONSTANT = "I cannot change"
+
+
+
+# operators
+
+a = 10
+b = 20
+
+
+# arithmetic
+
+sum = a + b # addition
+diff = a - b # subt
+prod = a * b #mult
+quot = a / b # division
+
+
+# comparison
+
+is_equal = (a == b) #equality
+is_not_equal = (a != b) #inequality
+is_greater = ( a > b) #greater than
+
+
+#logical
+
+and_op = (a < b && b > 10) #AND
+or_op = (a < b || b < 10) #OR
