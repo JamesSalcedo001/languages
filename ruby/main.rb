c1d6a93b5ee4fd90e8d1357c1d3769302890969e
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-23 14:05:17 -0500

    create ruby file, add data typesand variables sections

---
+++ ruby/main.rb
@@ -0,0 +1,15 @@
+# data types
+
+
+integer = 10 # integer
+float = 10.5 #float
+string = "Hello" #string
+boolean = true #boolean
+
+
+# variables
+
+
+variable = "I can change"
+
+CONSTANT = "I cannot change"
