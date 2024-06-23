1e7d5d877ab7d30e8f19337dd8152f4948547e8f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-23 15:56:19 -0500

    Add variables section and notes

--- go/main.go
+++ go/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 // note, cannot import using single quotes
 
-// data types
 
 func main() {
+
+	// data types
+
+
+
 	var integer int = 10 //integer, notice the usage of data types
 	var floatNum float64 = 10.5 // float, notice the datatype float64, as well as the usage of camelCase
 	var string string = "Hello"; // string, also note the optional ;
@@ -13,4 +17,24 @@ func main() {
 
 
 	fmt.Println(integer, floatNum, string, boolean)
+
+
+
+
+
+
+	// variables
+
+	var variable int = 10
+	// notice how variables are highlighted until they are used, it actually causes errors as well when running
+	const CONSTANT int = 20
+
+	fmt.Println(variable, CONSTANT)
+	
+
+
+
+
+
+
 }
