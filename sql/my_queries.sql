ecf839a768a3c51cf7c711082948af4d936256f4
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-28 20:08:52 -0500

    Add CRUD SQL statements

--- sql/my_queries.sql
+++ sql/my_queries.sql
@@ -1,16 +1,24 @@
+-- create a table
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
+    name VARCHAR(50),
+    age INT
 );
 
 
+-- read a table's records
+
+
+-- this displays all records in users table
 SELECT * FROM users;
 
+-- this displays the name and age of all records in users table with the condition that the age is greater than 28
+--
+SELECT name, age FROM users WHERE age > 28;
 
-INSERT INTO users (name) VALUES ('James');
 
-SELECT * FROM users;
+-- this updates an existing record, setting the record with the name 'John' to have an age attribute with the value of 30
+UPDATE users SET age = 30 WHERE name = 'John';
 
-INSERT INTO users (name) VALUES ('John');
 
-SELECT * FROM users;
