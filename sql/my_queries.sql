86c6f24cafc0b1700aba005556441b05cd981db4
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-28 21:00:42 -0500

    add INSERT INTO to populate table

--- sql/my_queries.sql
+++ sql/my_queries.sql
@@ -7,24 +7,30 @@ CREATE TABLE users (
 );
 
 
+-- this inserts information into an existing table
+INSERT INTO users (name, age) VALUES ('Binx', 3);
+
+INSERT INTO users (name, age) VALUES ('Tomo', 5);
+
+INSERT INTO users (name, age) VALUES ('Dhalia', 6);
+
 -- read a table's records
 
 
 -- this displays all records in users table
 SELECT * FROM users;
 
--- this displays the name and age of all records in users table with the condition that the age is greater than 28
---
-SELECT name, age FROM users WHERE age > 28;
+-- this displays the name and age of all records in users table with the condition that the age is greater than 3
+
+SELECT name, age FROM users WHERE age > 3;
 
 
--- this updates an existing record, setting the record with the name 'John' to have an age attribute with the value of 30
-UPDATE users SET age = 30 WHERE name = 'John';
+-- this updates an existing record, setting the record with the name 'Dhalia' to have an age attribute with the value of 4
+UPDATE users SET age = 4 WHERE name = 'Dhalia';
 
 
--- this deletes the user named John from the table
-DELETE FROM users WHERE name = 'John';
-
+-- this deletes the user named Dhalia from the table
+DELETE FROM users WHERE name = 'Dhalia';
 
 
 -- constraints
@@ -32,4 +38,5 @@ DELETE FROM users WHERE name = 'John';
     -- foreign key: references a primary key in another table
     -- unique: ensures all values in column are different
     -- not null: ensures a column cannot have null values
-    
+
+
