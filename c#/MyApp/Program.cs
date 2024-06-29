f820b212e3144b407c85f1c0f9de9cc174ee81ea
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-29 11:44:00 -0500

    Install dotnet app, add data types section for C#

---
+++ c#/MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+          // data types
+        
+        int integer = 10;
+        double floatNum = 10.5;
+        string str = "Hello";
+        bool boolean = true;
+
+
+
+
+        Console.WriteLine($"{integer}, {floatNum}, {str}, {boolean}");
+    }
+}
