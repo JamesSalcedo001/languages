d38f4ec7dddeebd28f7a37aab47605a6fd5126ad
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-06-29 19:08:25 -0500

    Add data types for C++

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,28 @@
+// to run code: compile with: g++ main.cpp -o main, and then ./main
+
+
 #include <iostream>
 using namespace std;
 
 
 int main() {
-    cout <<  "Hello World!";
+
+    // data types
+
+    int integer = 10;
+    float floatNum = 10.5f;
+    string str = "Hello";
+    bool boolean = true;
+
+
+
+
+
+
+
+
+
+    cout <<  integer << ", " << floatNum << ", " << str << ", " << boolean << endl;
+
+    return 0;
 }
