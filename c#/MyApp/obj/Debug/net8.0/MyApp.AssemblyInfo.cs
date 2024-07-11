2e5a7694f689c2643dd71c6ba7afa9e3bdd678bc
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-07-10 19:05:52 -0500

    updating

--- c#/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ c#/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df14b2f60b0cb985381a5fb1d691ad8a73329955")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e817ebd0472fa78c0fdc582f56091848edf08e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ javascript/learning_2/index.js
@@ -0,0 +1,43 @@
+/* 
+
+    features of JS
+
+    lightweight, interpreted, or just-in-time compiled language with first class functions. well know for being the scripting language of the web
+
+    use cases: web dev client and server side with Node.js, game dev, mobile app dev. and desktop apps.
+
+    JS Engines 
+
+    definition: JS engines are programs that execute JS code, examples include Google's V8 for chrome and Node.js, SpiderMonkey used in Firefox, and JavaScriptCore used in Safari
+
+
+    how they work: 
+
+    JS Engines interpret JS code into executable machine code, optimizing performance through techniques like Just-In-Time JIT compilation
+
+    JS in the browse 
+
+    client side scripting: JS can manipulate HTML and CSS to create dynamic web pages
+
+    browser compatibility: modern JS ES6+ is supported by all major browsers but it's essential to be aware of differences in previous versions
+
+    popular IDE's include: VSCode, Sublime Text, and Atom, which support syntax highlighting, code completion, and debugging for JS.
+
+    Dev tools: all major browsers have built in dev tools that allow JS debugging, inspect elements, and analyze network activity. 
+
+
+
+
+    NODE.js includes NPM or Node Package Manager, which is essential for managing JS packages
+
+    check versions with node -v and npm -v in terminal
+
+    creating proj directory 
+
+    mkdir name-of-project
+    cd name-of-project
+
+    
+
+
+*/
