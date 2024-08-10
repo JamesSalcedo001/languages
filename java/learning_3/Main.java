9b1d8f8659a3dd03e0ac8ea24b6a9ab87d80088e
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-10 17:49:14 -0500

    Show examples of multidimensional arrays

--- java/learning_3/Main.java
+++ java/learning_3/Main.java
@@ -2,14 +2,24 @@ import java.util.Arrays;
 import java.util.Date;
 
 import java.awt.*;
+import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = { 2, 3, 5, 1, 4};
-        // System.out.println(numbers.length);
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        // int[][] numbers = new int[2][3];
+        // // 2 dimensional array with 2 rows and 3 columns
+        // numbers[0][0] = 1;
+        // System.out.println(Arrays.deepToString(numbers));
+
+        // int[][][] numbers = new int[2][3][5];
+        // // 3 dimensional array with 2 rows and 3 columns
+
+        int[][] numbers = { {1, 2, 3}, {4, 5, 6} };
+        // 2 dimensional array
+        
+        System.out.println(Arrays.deepToString(numbers));
+
 
     }
 }
