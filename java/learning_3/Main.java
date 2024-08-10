98f0e096088a304fd92b061ba9790ec619129860
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-10 17:51:21 -0500

    Show constant usage

--- java/learning_3/Main.java
+++ java/learning_3/Main.java
@@ -7,18 +7,9 @@ import java.lang.reflect.Array;
 public class Main {
     public static void main(String[] args) {
 
-        // int[][] numbers = new int[2][3];
-        // // 2 dimensional array with 2 rows and 3 columns
-        // numbers[0][0] = 1;
-        // System.out.println(Arrays.deepToString(numbers));
-
-        // int[][][] numbers = new int[2][3][5];
-        // // 3 dimensional array with 2 rows and 3 columns
-
-        int[][] numbers = { {1, 2, 3}, {4, 5, 6} };
-        // 2 dimensional array
-        
-        System.out.println(Arrays.deepToString(numbers));
+       // final sets variables to constants
+       final float PI = 3.14F;
+       
 
 
     }
