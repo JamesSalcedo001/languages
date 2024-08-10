03323ebac8d2113e20ab8bc31c794c396540bb00
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-10 17:41:58 -0500

    Demonstrate newer array syntax and array sort method, as well as array toString method to use println

--- java/learning_3/Main.java
+++ java/learning_3/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Date;
 
 import java.awt.*;
@@ -5,16 +6,10 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        String message = "  Hello World" + "!!     ";
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("H"));
-        System.out.println(message.indexOf("sky"));
-        System.out.println(message.replace("!", "*"));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.trim());
+        int[] numbers = { 2, 3, 5, 1, 4};
+        // System.out.println(numbers.length);
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
