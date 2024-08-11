bc5918b785bf909b444a791044c21a77859777d3
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-10 19:54:10 -0500

    For loops incrementing decrementing and different uses for the conditions

--- java/learning_3/Main.java
+++ java/learning_3/Main.java
@@ -2,21 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Hello World");
+        // }
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println("Hello World " + i);
+        // }
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Hello World " + i);
+        // }
 
-        if (number % 5 == 0 && number % 3 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 5 == 0) {
-            System.out.println("Fizz");
-        } else if (number % 3 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
+        for (int i = 5; i > 5; i--) {
+            System.out.println("Hello World " + i);
         }
-
-
        
     }
 }
