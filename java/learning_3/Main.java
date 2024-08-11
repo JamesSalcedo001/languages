506be439e00c22710a9b57f2a1f6b2dc491162ba
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-10 19:10:38 -0500

    Build out mortgage calculator

--- java/learning_3/Main.java
+++ java/learning_3/Main.java
@@ -1,32 +1,29 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Enter your age: ");
-        // byte age = scanner.nextByte();
-        // System.out.println("You are " + age);
+       final byte MONTHS_IN_YEAR = 12;
+       final byte PERCENT = 100;
 
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Enter your age: ");
-        // float age = scanner.nextFloat();
-        // System.out.println("You are " + age);
+       Scanner scanner = new Scanner(System.in);
 
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Enter your age: ");
-        // double age = scanner.nextDouble();
-        // System.out.println("You are " + age);
+       System.out.print("Principal: ");
+       int principal = scanner.nextInt();
 
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.print("Enter your name: ");
-        // String name = scanner.next();
-        // // next method reads one token, if you type words with space it will only read the first one as that counts as two tokens, and the second time it will read the second token
-        // System.out.println("You are " + name);
+       System.out.print("Annual interest rate: ");
+       float annualInterest = scanner.nextFloat();
+       float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine().trim();
-        // next method reads one token, if you type words with space it will only read the first one as that counts as two tokens, and the second time it will read the second token
-        System.out.println("You are " + name);
+       System.out.print("Period (Years): ");
+       byte years = scanner.nextByte();
+       int numberOfPayments = years * MONTHS_IN_YEAR;
+
+       double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
