e9e5b1c8558a2795bbac1617dd6a2e0920e37dad
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-14 13:19:04 -0500

    Demonstrate usage of Wrapper classes

--- java/learning_4/Main.java
+++ java/learning_4/Main.java
@@ -1,30 +1,33 @@
+// wrapper classes - provides a way to use primitive data types as reference data types, contain useful methods, can be used with collections (ex: ArrayList)
+
+
+// primitive    // wrapper
+// ............ // ........
+/*
+    boolean     Boolean
+    char        Character
+    int         Integer
+    double      Double
+
+    autoboxing = the automatic conversion that the Java compiler makes between the primitive data types and their corresponding object wrapper classes
+
+    unboxing = the reverse of autoboxing, automatic conversion of wrapper class to primitive
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // String reference data types that can store one or more characters reference data types have access to useful methods
+        
+        Boolean a = true;
+        Character b = '@';       
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "James";
 
 
-        String name = "James";
-
-        // boolean result = name.equals("James");
-        // boolean result = name.equalsIgnoreCase("james");
-
-        // int result = name.length();
-
-        // char result = name.charAt(0);
-
-        // int result = name.indexOf("J");
-
-        // boolean result = name.isEmpty();
-
-        // String result = name.toUpperCase();
-        // String result = name.toLowerCase();
-
-        // String result = name.trim();
-
-        String result = name.replace('J', 'Z');
-
-
-        System.out.println(result);
+        // unboxing to a primitive data type
+        if (a==true) {
+            System.out.println("This is true");
+        }
 
     }
 }
@@ -51,6 +54,53 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         // String reference data types that can store one or more characters reference data types have access to useful methods
+
+
+//         String name = "James";
+
+//         // boolean result = name.equals("James");
+//         // boolean result = name.equalsIgnoreCase("james");
+
+//         // int result = name.length();
+
+//         // char result = name.charAt(0);
+
+//         // int result = name.indexOf("J");
+
+//         // boolean result = name.isEmpty();
+
+//         // String result = name.toUpperCase();
+//         // String result = name.toLowerCase();
+
+//         // String result = name.trim();
+
+//         String result = name.replace('J', 'Z');
+
+
+//         System.out.println(result);
+
+//     }
+// }
 
 
 
