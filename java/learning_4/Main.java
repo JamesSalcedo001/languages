f18e278e57e7710b0b5f5355425d0756802dd964
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-14 11:12:31 -0500

    Add AND and OR logical operators

--- java/learning_4/Main.java
+++ java/learning_4/Main.java
@@ -1,11 +1,49 @@
 // and or and not logical operators
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // AND
-        // OR
+        // logical ops - connect two or more expressions
+
         // NOT
+
+
+
+
+        // AND
+
+
+        int temp = 25;
+
+        if (temp > 30) {
+            System.out.println("It is hot");
+        } 
+        else if (temp >= 20 && temp <= 30) {
+            System.out.println("It is warm");
+        }
+        else {
+            System.out.println("It is cold");
+        }
+
+
+
+        // OR
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game! press q or Q to quit");
+
+        String response = scanner.next();
+
+        if (response.equals("q") || response.equals("Q")) {
+            System.out.println("You quit the game");
+        }
+        else {
+            System.out.println("You are still playing the game!");
+        }
+
+
     }
 }
 
