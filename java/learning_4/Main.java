142eb68d2a55ef244925da00b049cdd5eee98ae3
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-14 21:26:10 -0500

    OOP: Contruct Car instance

--- java/learning_4/Main.java
+++ java/learning_4/Main.java
@@ -1,49 +1,107 @@
-// overloaded methods
+// OOP contructing car instances
 
 
 public class Main {
+
     public static void main(String[] args) {
-            // overloaded methods - share the same name but have different params, method name + params = method signature
-            
-          
-            double x = add(1.0, 2.0, 3.0, 4.0);
-            System.out.println(x);
-            
+        // object - instance of a class that may contain attributes and methods
 
-    }
-
-    static int add(int a, int b) {
-        System.out.println("This is overloaded method #1");
-        return a + b;
-    }
-
-    static int add(int a, int b, int c) {
-        System.out.println("This is overloaded method #2");
-        return a + b + c;
-    }
-
-    static int add(int a, int b, int c, int d) {
-        System.out.println("This is overloaded method #3");
-        return a + b + c + d;
-    }
+         Car myCar = new Car();
+         Car myCar2 = new Car();
 
 
-    static double add(double a, double b) {
-        System.out.println("This is overloaded method #4");
-        return a + b;
-    }
+         System.out.println(myCar.model);
+         System.out.println(myCar.make);
 
-    static double add(double a, double b, double c) {
-        System.out.println("This is overloaded method #5");
-        return a + b + c;
-    }
+         System.out.println();
 
-    static double add(double a, double b, double c, double d) {
-        System.out.println("This is overloaded method #6");
-        return a + b + c + d;
+         System.out.println(myCar2.make);
+         System.out.println(myCar.model);
+
+         myCar.drive();
+
+         myCar.brake();
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// overloaded methods
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//             // overloaded methods - share the same name but have different params, method name + params = method signature
+            
+          
+//             double x = add(1.0, 2.0, 3.0, 4.0);
+//             System.out.println(x);
+            
+
+//     }
+
+//     static int add(int a, int b) {
+//         System.out.println("This is overloaded method #1");
+//         return a + b;
+//     }
+
+//     static int add(int a, int b, int c) {
+//         System.out.println("This is overloaded method #2");
+//         return a + b + c;
+//     }
+
+//     static int add(int a, int b, int c, int d) {
+//         System.out.println("This is overloaded method #3");
+//         return a + b + c + d;
+//     }
+
+
+//     static double add(double a, double b) {
+//         System.out.println("This is overloaded method #4");
+//         return a + b;
+//     }
+
+//     static double add(double a, double b, double c) {
+//         System.out.println("This is overloaded method #5");
+//         return a + b + c;
+//     }
+
+//     static double add(double a, double b, double c, double d) {
+//         System.out.println("This is overloaded method #6");
+//         return a + b + c + d;
+//     }
+    
+// }
 
 
 
