51c9aafc79c8241afda5fd42bf0ccef130595420
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-14 21:13:25 -0500

    Demonstrate overloaded methods

--- java/learning_4/Main.java
+++ java/learning_4/Main.java
@@ -1,23 +1,48 @@
-import java.util.ArrayList;
+// overloaded methods
+
 
 public class Main {
-
     public static void main(String[] args) {
-         // foreach loop = traversing technique to iterate through elems in an array/collection, less steps more readable, less flexible though
+            // overloaded methods - share the same name but have different params, method name + params = method signature
+            
+          
+            double x = add(1.0, 2.0, 3.0, 4.0);
+            System.out.println(x);
+            
 
-        //  String[] animals = { "cat", "dog", "rat", "bird"};
-
-        ArrayList<String> animals = new ArrayList<String>();
-
-        animals.add("Cat");
-        animals.add("Dog");
-        animals.add("Rat");
-        animals.add("Bird");
-
-         for (String i : animals) {
-            System.out.println(i);
-         }
     }
+
+    static int add(int a, int b) {
+        System.out.println("This is overloaded method #1");
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        System.out.println("This is overloaded method #2");
+        return a + b + c;
+    }
+
+    static int add(int a, int b, int c, int d) {
+        System.out.println("This is overloaded method #3");
+        return a + b + c + d;
+    }
+
+
+    static double add(double a, double b) {
+        System.out.println("This is overloaded method #4");
+        return a + b;
+    }
+
+    static double add(double a, double b, double c) {
+        System.out.println("This is overloaded method #5");
+        return a + b + c;
+    }
+
+    static double add(double a, double b, double c, double d) {
+        System.out.println("This is overloaded method #6");
+        return a + b + c + d;
+    }
+    
 }
 
 
@@ -28,6 +53,62 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// array/collection iteration with foreach loop
+
+// import java.util.ArrayList;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//          // foreach loop = traversing technique to iterate through elems in an array/collection, less steps more readable, less flexible though
+
+//         //  String[] animals = { "cat", "dog", "rat", "bird"};
+
+//         ArrayList<String> animals = new ArrayList<String>();
+
+//         animals.add("Cat");
+//         animals.add("Dog");
+//         animals.add("Rat");
+//         animals.add("Bird");
+
+//          for (String i : animals) {
+//             System.out.println(i);
+//          }
+//     }
+// }
 
 
 
