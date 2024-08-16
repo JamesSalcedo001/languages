44d432d531ec5caec582de0c50238ba295168f94
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-16 17:21:31 -0500

    Overloaded constructors pt 1

--- java/learning_4/Pizza.java
+++ java/learning_4/Pizza.java
@@ -1,5 +1,15 @@
 public class Pizza {
-    Pizza() {
-        
+    
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+    
+    Pizza(String bread, String sauce, String cheese, String topping) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+         
     }
 }
