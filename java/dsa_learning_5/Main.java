9a4887bfd6d2a52e5667e8f2dd7940b50bec5682
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-17 20:20:10 -0500

    Build out Dynamic array with methods pt3

--- java/dsa_learning_5/Main.java
+++ java/dsa_learning_5/Main.java
@@ -7,11 +7,23 @@ public class Main {
     public static void main(String[] args) {
         // ArrayList<String> arrayList = new ArrayList<>();
 
-        DynamicArray dynamicArray = new DynamicArray();
+        DynamicArray dynamicArray = new DynamicArray(5);
 
         dynamicArray.add("A");
         dynamicArray.add("B");
         dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+
+        // dynamicArray.insert(0, "X");
+        dynamicArray.delete("A");
+        dynamicArray.delete("B");
+        dynamicArray.delete("C");
+
+
+
+        // System.out.println(dynamicArray.search("C"));
 
         System.out.println(dynamicArray);
         System.out.println("empty: " + dynamicArray.isEmpty());
