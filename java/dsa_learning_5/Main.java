828a60469150e882616414ea90c4798556978ff9
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-18 14:57:19 -0500

    Linear search algorithm pt1

--- java/dsa_learning_5/Main.java
+++ java/dsa_learning_5/Main.java
@@ -1,81 +1,140 @@
-// linkedLists vs arraylists, most applications arraylist performs better, however with larger data sets, when inserting or deleting elements, linkedLists perform better
+/*
+    linear search = Iterate through a collection one element at a time
+
+    runtime complexity: O(n) (linear time)
+
+    disadvantages: 
+
+    slow for large data sets
+
+    advantages:
+    fast for searches of small to medium data sets
+    does not need to be sorted
+    useful for data structures that do not have random access (linked list)
+
+
+ 
+ */
 
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-
-
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-
-        long startTime;
-        long endTime;
-        long elapsedTime;
-
-        for (int i = 0; i < 1000000; i++) {
-            linkedList.add(i);
-            arrayList.add(i);
-        }
-
-        // ************* LinkedList *************
-
-        startTime = System.nanoTime();
-
-        // linkedList.get(0);
-        // linkedList.get(500000);
-        // linkedList.get(999999);
-        // linkedList.remove(0);
-        // linkedList.remove(500000);
-        linkedList.remove(999999);
-
-
-        // do something
-
-        endTime = System.nanoTime();
-
-        elapsedTime = endTime - startTime;
-
-        System.out.println("LinkedList:\t" + elapsedTime + " ns");
-
-
-
-
-
-        // ************* ArrayList *************
-
-
-        startTime = System.nanoTime();
-
-        // arrayList.get(0);
-
-        // arrayList.get(500000);
-
-        // arrayList.get(999999);
-
-        // arrayList.remove(0);
-        // arrayList.remove(500000);
-
-        arrayList.remove(999999);
-
-        // do something
-
-        endTime = System.nanoTime();
-
-        elapsedTime = endTime - startTime;
-
-        System.out.println("ArrayList:\t" + elapsedTime + " ns");
-
-
+        
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// linkedLists vs arraylists, most applications arraylist performs better, however with larger data sets, when inserting or deleting elements, linkedLists perform better
+
+
+
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+
+
+//         LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+
+//         ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+//         long startTime;
+//         long endTime;
+//         long elapsedTime;
+
+//         for (int i = 0; i < 1000000; i++) {
+//             linkedList.add(i);
+//             arrayList.add(i);
+//         }
+
+//         // ************* LinkedList *************
+
+//         startTime = System.nanoTime();
+
+//         // linkedList.get(0);
+//         // linkedList.get(500000);
+//         // linkedList.get(999999);
+//         // linkedList.remove(0);
+//         // linkedList.remove(500000);
+//         linkedList.remove(999999);
+
+
+//         // do something
+
+//         endTime = System.nanoTime();
+
+//         elapsedTime = endTime - startTime;
+
+//         System.out.println("LinkedList:\t" + elapsedTime + " ns");
+
+
+
+
+
+//         // ************* ArrayList *************
+
+
+//         startTime = System.nanoTime();
+
+//         // arrayList.get(0);
+
+//         // arrayList.get(500000);
+
+//         // arrayList.get(999999);
+
+//         // arrayList.remove(0);
+//         // arrayList.remove(500000);
+
+//         arrayList.remove(999999);
+
+//         // do something
+
+//         endTime = System.nanoTime();
+
+//         elapsedTime = endTime - startTime;
+
+//         System.out.println("ArrayList:\t" + elapsedTime + " ns");
+
+
+//     }
+// }
 
 
 
