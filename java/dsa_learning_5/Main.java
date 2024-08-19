fa24aba830656c8199619f994700f4628187294f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-18 21:07:27 -0500

    Update

--- java/dsa_learning_5/Main.java
+++ java/dsa_learning_5/Main.java
@@ -1,3 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
     Insertion sort - after comparing elements to the left, shift elements to the right to make room to insert a value
@@ -17,52 +53,33 @@
  */
 
 
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        int array[] = {9, 1, 8, 2, 7, 3, 6, 5, 4};
+//         int array[] = {9, 1, 8, 2, 7, 3, 6, 5, 4};
 
-        insertionSort(array);
+//         insertionSort(array);
 
-        for (int i : array) {
-            System.out.print(i + " ") ;
-        }
+//         for (int i : array) {
+//             System.out.print(i + " ") ;
+//         }
         
-    }
+//     }
 
-    private static void insertionSort(int[] array) {
-       for (int i = 1; i < array.length; i++) {
-            int temp = array[i];
-            int j = i - 1;
+//     private static void insertionSort(int[] array) {
+//        for (int i = 1; i < array.length; i++) {
+//             int temp = array[i];
+//             int j = i - 1;
 
-            while (j >= 0 && array[j] > temp) {
-                array[j + 1] = array[j];
-                j--;
-            }
-            array[j + 1] = temp;
-       } 
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//             while (j >= 0 && array[j] > temp) {
+//                 array[j + 1] = array[j];
+//                 j--;
+//             }
+//             array[j + 1] = temp;
+//        } 
+//     }
+// }
 
 
 
