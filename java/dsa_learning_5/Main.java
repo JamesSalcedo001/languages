70f921ab908faa156cf34f87eb7b41d7cf4a1975
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-21 11:37:14 -0500

    Recursion usage pt7

--- java/dsa_learning_5/Main.java
+++ java/dsa_learning_5/Main.java
@@ -48,15 +48,29 @@
 // }
 
 
+// public class Main {
+
+//     public static void main(String[] args) {
+//         System.out.println(factorial(7));
+//     }
+
+//     private static int factorial(int num) {
+//         if (num < 1) return 1; // base case
+//         return num * factorial(num - 1); // recursive
+//     }
+// }
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(factorial(7));
+        System.out.println(power(2, 8));
     }
 
-    private static int factorial(int num) {
-        if (num < 1) return 1; // base case
-        return num * factorial(num - 1); // recursive
+    private static int power(int base, int exponent) {
+        if (exponent < 1) return 1; // base case
+        return base * power(base, exponent - 1);
     }
 }
 
