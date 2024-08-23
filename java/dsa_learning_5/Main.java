d5f475f8429a5ef63c4817eac666c4cbfb47d28c
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-23 15:20:34 -0500

    Merge Sort usage pt2

--- java/dsa_learning_5/Main.java
+++ java/dsa_learning_5/Main.java
@@ -7,7 +7,46 @@
 public class Main {
 
     public static void main(String[] args) {
-         
+         int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
+
+         mergeSort(array);
+
+         for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+         }
+    }
+
+    private static void mergeSort(int[] array) {
+        int length = array.length;
+        if (length <= 1) return; // base case
+
+        int middle = length / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+        int i = 0; // left array
+        int j = 0; // right array
+
+        for (; i < length; i++) {
+            if (i < middle) {
+                leftArray[i] = array[i];
+            }
+            else {
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
+    }
+
+    // helper method
+    private static void merge(int[] leftArray, int[] rightArray, int[] array) {
+
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+
     }
 }
 
