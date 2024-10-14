a65ae06cc75efb138da19b559c57b6ce48a09c6b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-13 20:26:09 -0500

    Day Two Text Editor - create highlighter function

--- javascript/hundred_days_js/day_two/index.js
+++ javascript/hundred_days_js/day_two/index.js
@@ -68,3 +68,22 @@ linkButton.addEventListener("click", () => {
         modifyText(linkButton.id, false, userLink);
     }
 });
+
+const highLighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (needsRemoval) {
+                let alreadyActive = false;
+                if (button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+                highLighterRemover(className);
+                if (!alreadyActive) {
+                    button.classList.add("active");
+                }
+            } else {
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
