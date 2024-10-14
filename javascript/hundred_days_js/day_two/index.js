726e9cc7346a71f7cd61a4fa73f335860144effa
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-13 19:51:49 -0500

    Day Two Text Editor - select elements using javascript pt1

---
+++ javascript/hundred_days_js/day_two/index.js
@@ -0,0 +1,7 @@
+let optionButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRed = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alingButtons = document.querySelectorAll(".align");
