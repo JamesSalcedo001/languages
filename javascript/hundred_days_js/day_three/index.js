53f6adfb2672a6559d35e14dc4f56e268521399b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-14 22:31:20 -0500

    Password Generator - fix bug with script tag using src

--- javascript/hundred_days_js/day_three/index.js
+++ javascript/hundred_days_js/day_three/index.js
@@ -1,6 +1,6 @@
 const lengthSlider = document.querySelector(".pass-length input");
 const options = document.querySelectorAll(".option input");
-const copyIcon = document.querySelectot(".input-box span");
+const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
 const passIndicator = document.querySelector(".pass-indicator");
 const generateBtn = document.querySelector(".generate-btn");
