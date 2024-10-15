3199f54078e26d114c45377dd91d3d328c6e3aac
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-14 21:45:00 -0500

    Password Generator - select necessary elements with JavaScript's document.querySelector

---
+++ javascript/hundred_days_js/day_three/index.js
@@ -0,0 +1,8 @@
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelectot(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
+
+
