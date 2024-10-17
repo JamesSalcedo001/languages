46fdeb889633b484727e6442a845ff88502c2be0
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-17 18:34:03 -0500

    Add create, read, and update actions to controller

--- projects/calculator_app_js_node/controllers/calculatorController.js
+++ projects/calculator_app_js_node/controllers/calculatorController.js
@@ -1,5 +1,8 @@
 // calculatorController.js
+const CalculationModel = require("../models/calculationModel");
 
+
+// perform calculation(create)
 exports.performCalculation = (req, res) => {
     const { firstOperand, operator, secondOperand } = req.body;
     let result;
@@ -32,8 +35,60 @@ exports.performCalculation = (req, res) => {
             return res.status(400).json("Invalid operator");
     }
 
+    const newCalculation = CalculationModel.create(firstOperand, operator, secondOperand, result);
+
     console.log(`Calculation successful: Result = ${result}`);
 
-    // return result as a JSON response with 200 status
-    res.status(200).json({ result: result.toString() });
-}
+    res.status(201).json(newCalculation); // send back new calculation
+};
+
+
+
+// retrieve all calculations (Read)
+
+exports.getAllCalculations = (req, res) => {
+    const calculations = CalculationModel.getAll();
+    res.status(200).json(calculations);
+};
+
+// update existing calculation (Update)
+exports.updateCalculation = (re, res) => {
+    const { id } = req.params;
+    const { firstOperand, operator, secondOperand } = req.body;
+    let result;
+
+    // perform calculation with updated data
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(secondOperand);
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                return res.status(400).json({ error: "Cannot divide by zero" });
+            }
+            break;
+        default:
+            return res.status(400).json({ error: "Invalid operator" });
+    }
+
+    const updatedCalculation = CalculationModel.update(id, firstOperand, operator, secondOperand, result);
+
+    if (!updatedCalculation) {
+        return res.status(404).json({ error: "Calculation not found" });
+    }
+
+    res.status(200).json(updatedCalculation);
+};
+
+
