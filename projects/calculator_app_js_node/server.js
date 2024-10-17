60162405d69d4281e637c84b3c07803470b8a325
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-17 15:07:23 -0500

    use express, cors, and middleware to parse json

--- projects/calculator_app_js_node/server.js
+++ projects/calculator_app_js_node/server.js
@@ -3,3 +3,9 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
+
+// middleware to parse JSON
+app.use(express.json());
+
+// allow CORS for front end to communicate with back end
+app.use(cors());
