7cec22a333a116a48fcdd0799ae0f2b0773fd2d9
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-17 14:42:14 -0500

    JS NODE CALCULATOR - set up event listener for buttons pt2

--- projects/calculator_app_js_node/public/index.js
+++ projects/calculator_app_js_node/public/index.js
@@ -7,5 +7,17 @@ let firstOperand = "";
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.textContent;
+
+        if (value >= "0" && value <= "9" || value === ".") {
+            // handle number input
+            currentInput += value;
+            display.value = currentInput;
+        } else if (value === "C") {
+            // clear display
+            currentInput = "";
+            firstOperand = "";
+            operator = "";
+            display.value = "";
+        }
     })
 })
