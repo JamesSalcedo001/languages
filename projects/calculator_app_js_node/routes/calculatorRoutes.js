3a402a1699647a414843c2db22d22ed0ea0df150
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-17 18:39:38 -0500

    Update router to handle CRUD ops

--- projects/calculator_app_js_node/routes/calculatorRoutes.js
+++ projects/calculator_app_js_node/routes/calculatorRoutes.js
@@ -7,4 +7,9 @@ const calculatorController = require("../controllers/calculatorController");
 // route to handle calculations
 router.post("/calculate", calculatorController.performCalculation);
 
+// CRUD routes for calc history
+router.get("/history", calculatorController.getAllCalculations);
+router.put("/history/:id", calculatorController.updateCalculation);
+router.delete("/history/:id", calculatorController.deleteCalculation);
+
 module.exports = router;
