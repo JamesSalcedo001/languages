a1217a64af34e88768b62c4569041875c71d3e31
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-23 09:04:27 -0500

    Finish Calculation.js

--- projects/calculator_app_js_node/models/Calculation.js
+++ projects/calculator_app_js_node/models/Calculation.js
@@ -3,3 +3,23 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
+const Calculation = sequelize.define("Calculation", {
+    firstOperand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    operator: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    secondOperand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    result: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+});
+
+module.exports = Calculation;
