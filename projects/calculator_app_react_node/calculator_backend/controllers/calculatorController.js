2655cf94cef19d8a53294818fe01db84fd46eacb
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-27 16:25:30 -0500

    Finish computeResult helper function

--- projects/calculator_app_react_node/calculator_backend/controllers/calculatorController.js
+++ projects/calculator_app_react_node/calculator_backend/controllers/calculatorController.js
@@ -6,4 +6,26 @@ const Calculation = require("../models/Calculation");
 
 function computeResult(num1, operator, num2) {
     let result;
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                throw new Error("Cannot divide by zero");
+            }
+            result = num1 / num2;
+            break;
+        default:
+            throw new Error("invalid operator");
+    }
+
+    return result;
 }
