af2569fc97ea48dd5cec8986cc036b0bd31cb9b1
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-27 16:22:30 -0500

    Begin computeResult function

--- projects/calculator_app_react_node/calculator_backend/controllers/calculatorController.js
+++ projects/calculator_app_react_node/calculator_backend/controllers/calculatorController.js
@@ -1,2 +1,9 @@
 // controllers/calculatorController.js
 
+const Calculation = require("../models/Calculation");
+
+// helper function to perform calculations
+
+function computeResult(num1, operator, num2) {
+    let result;
+}
