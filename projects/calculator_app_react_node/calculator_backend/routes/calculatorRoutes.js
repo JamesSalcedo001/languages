6eddc8540540358273d50cf26e0db9e0dc5f84f2
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-28 18:09:50 -0500

    Finish routes

--- projects/calculator_app_react_node/calculator_backend/routes/calculatorRoutes.js
+++ projects/calculator_app_react_node/calculator_backend/routes/calculatorRoutes.js
@@ -5,3 +5,9 @@ const router = express.Router();
 const calculatorController = require("../constorllers/calculatorController");
 
 router.post("/calculations", calculatorController.createCalculation);
+router.get("/calculations", calculatorController.getAllCalculations);
+router.get("/calculations/:id", calculatorController.getCalculationById);
+router.put("/calculations/:id", calculatorController.updateCalculation);
+router.get("/calculations/:id", calculatorController.deleteCalculation);
+
+module.exports = router;
