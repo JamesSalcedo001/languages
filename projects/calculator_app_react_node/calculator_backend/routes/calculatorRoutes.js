08aed090f49be0c849f34ec78312e6f1973168da
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-28 19:58:10 -0500

    Updating from require syntax to import syntax pt2

--- projects/calculator_app_react_node/calculator_backend/routes/calculatorRoutes.js
+++ projects/calculator_app_react_node/calculator_backend/routes/calculatorRoutes.js
@@ -1,8 +1,8 @@
 // routes/calculatorRoutes.js
 
-const express = require("express");
+import express from "express";
+import * as calculatorController from "../controllers/calculatorController.js";
 const router = express.Router();
-const calculatorController = require("../controllers/calculatorController");
 
 router.post("/calculations", calculatorController.createCalculation);
 router.get("/calculations", calculatorController.getAllCalculations);
@@ -10,4 +10,4 @@ router.get("/calculations/:id", calculatorController.getCalculationById);
 router.put("/calculations/:id", calculatorController.updateCalculation);
 router.get("/calculations/:id", calculatorController.deleteCalculation);
 
-module.exports = router;
+export default router;
