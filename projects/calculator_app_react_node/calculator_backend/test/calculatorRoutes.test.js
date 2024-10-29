fe2ee13ca37820913acb522b7e6bdba9c9adce8a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-28 21:16:44 -0500

    Start building out test file for calculatorRoutes pt 2

--- projects/calculator_app_react_node/calculator_backend/test/calculatorRoutes.test.js
+++ projects/calculator_app_react_node/calculator_backend/test/calculatorRoutes.test.js
@@ -6,3 +6,13 @@ import app from "../server";
 import sequelize from "../config/database";
 import Calculation from "../models/Calculation";
 
+describe("Calculator API Ednpoints", () => {
+    // before running tests sync the database
+    before(async () => {
+        await sequelize.sync({ force: true });
+    });
+
+    after(async () => {
+        await sequelize.close();
+    });
+})
