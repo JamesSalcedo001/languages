56487c7821ddf9779fdabee12ce239c8aac6ac85
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-28 19:15:23 -0500

    calculatorController test pt1

--- projects/calculator_app_react_node/calculator_backend/test/calculatorController.test.js
+++ projects/calculator_app_react_node/calculator_backend/test/calculatorController.test.js
@@ -1,1 +1,11 @@
 // test
+
+const { expect } = require("chai");
+const { computeResult } = require("../controllers/calculatorController");
+
+describe("computeResult Function", () => {
+    it("should add two numbers correctly", () => {
+        const result = computeResult(2, "+", 3);
+        expect(result).to.equal(5);
+    })
+})
