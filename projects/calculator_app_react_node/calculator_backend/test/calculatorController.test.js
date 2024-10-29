e8d36fefe4bf727b21dff8aeab11ff58035c470b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-28 19:20:11 -0500

    calculatorController test pt2

--- projects/calculator_app_react_node/calculator_backend/test/calculatorController.test.js
+++ projects/calculator_app_react_node/calculator_backend/test/calculatorController.test.js
@@ -7,5 +7,22 @@ describe("computeResult Function", () => {
     it("should add two numbers correctly", () => {
         const result = computeResult(2, "+", 3);
         expect(result).to.equal(5);
+    });
+
+    it("should subtract two numbers correctly", () => {
+        const result = computeResult(5, "-", 2);
+        expect(result).to.equal(3);
     })
+
+    it("should multiply two numbers correctly", () => {
+        const result = computeResult(4, "*", 3);
+        expect(result).to.equal(12);
+    });
+
+    it("should divide two numbers correctly", () => {
+        const result = computeResult(10, "/", 2);
+        expect(result).to.equal(5);
+    });
+
+    
 })
