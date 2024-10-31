5502484011680ea7855f8f7bfa5040238c0c8159
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-30 21:50:33 -0500

    Bug fix pt3

--- projects/calculator_app_react_node/calculator_backend/config/database.js
+++ projects/calculator_app_react_node/calculator_backend/config/database.js
@@ -4,14 +4,37 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    development: {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         logging: false,
+    },
+    test: {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+    },
+};
+
+const currentConfig = config[env];
+
+const sequelize = new Sequelize(
+    currentConfig.database,
+    currentConfig.username,
+    currentConfig.password,
+    {
+        host: currentConfig.host,
+        dialect: currentConfig.dialect,
+        logging: currentConfig.logging,
     }
 
 );
