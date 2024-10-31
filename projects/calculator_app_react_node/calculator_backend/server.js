5f34b486b3aa2efc1f47098231accb6d9507d737
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-30 19:34:31 -0500

    Add log telling which port swagger docs are served on

--- projects/calculator_app_react_node/calculator_backend/server.js
+++ projects/calculator_app_react_node/calculator_backend/server.js
@@ -8,7 +8,7 @@ import sequelize from './config/database.js';
 import calculatorRoutes from './routes/calculatorRoutes.js';
 
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger_output.json" asser { type: "json" };
+import swaggerFile from "./swagger_output.json" assert { type: "json" };
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV !== "test") {
   
       app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
+        console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
       });
     } catch (error) {
       console.error('Unable to connect to the database:', error);
