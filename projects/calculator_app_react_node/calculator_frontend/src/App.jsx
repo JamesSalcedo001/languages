04f5bd50174dc8f5fd2110e7ad9494e12b197d03
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-31 22:13:43 -0500

    Create file Calculator.jsx and start setting up component, import Calculator.jsx to App.jsx and start setting up state

--- projects/calculator_app_react_node/calculator_frontend/src/App.jsx
+++ projects/calculator_app_react_node/calculator_frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Calculator } from './components/Calculator';
 
 import './App.css'
 
 function App() {
+  const [calculations, setCalculations] = useState([]);
 
   return (
-    <>
-    </>
+    <div className='app'>
+      <h1>Calculator App</h1>
+      <Calculator />
+    </div>
   )
 }
 
---
+++ projects/calculator_app_react_node/calculator_frontend/src/components/Calculator.jsx
@@ -0,0 +1,8 @@
+// src/components/Calculator.js
+export function Calculator() {
+    return (
+        <div className="calculator">
+            <h1>Calculator</h1>
+        </div>
+    )
+}
