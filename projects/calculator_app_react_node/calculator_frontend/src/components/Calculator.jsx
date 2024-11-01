384f242264f1ad968d2759493e4ca9e0a0b23b93
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-31 22:53:53 -0500

    Add state and calculator element pt1

--- projects/calculator_app_react_node/calculator_frontend/src/components/Calculator.jsx
+++ projects/calculator_app_react_node/calculator_frontend/src/components/Calculator.jsx
@@ -1,8 +1,23 @@
 // src/components/Calculator.js
-export function Calculator() {
+
+import { useState } from "react";
+
+export function Calculator({ onNewCalculation }) {
+    const [firstOperand, setFirstOperand] = useState("");
+    const [operator, setOperator] = useState("");
+
+
     return (
         <div className="calculator">
             <h1>Calculator</h1>
+            <div>
+                <input 
+                    type="number"
+                    placeholder="First Operand"
+                    value={firstOperand}
+                    onChange={(e) => setFirstOperand(e.target.value)}
+                />
+            </div>
         </div>
     )
 }
