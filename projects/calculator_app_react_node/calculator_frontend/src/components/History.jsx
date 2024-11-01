5e50509302ba66d5a186105690f1a42973866faf
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-10-31 23:25:37 -0500

    Use map to transform and display calculation history

--- projects/calculator_app_react_node/calculator_frontend/src/components/History.jsx
+++ projects/calculator_app_react_node/calculator_frontend/src/components/History.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable react/prop-types */
 // src/components/History.js
 
 
 export function History({ calculations }) {
     return (
         <div className="history">
-
+            <h2>Calculation History</h2>
+            {calculations.length === 0 ? (
+                <p>No calculations yet</p>
+            ) : (
+                <ul>
+                    {calculations.map((calc) => (
+                        <li key={calc.id}>
+                            {calc.firstOperand} {calc.operator} {calc.secondOperand} = {calc.result}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
