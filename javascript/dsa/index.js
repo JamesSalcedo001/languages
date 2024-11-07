79d9b16efba4ee0128fadd839e1d23b79c0bc5c9
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-06 22:07:08 -0600

    Add Array implementation ex1

--- javascript/dsa/index.js
+++ javascript/dsa/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// arrays - collection of elements stored in a contiguous block of memory. elems are accessible by index
+
+// simple use
+
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray[0]); // access first element
+
+myArray.push(6); // add an element to the end: [1, 2, 3, 4, 5, 6]
+
+myArray.pop(); // remove the last element: [1, 2, 3, 4, 5]
+
