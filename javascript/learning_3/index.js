a15086e5b284f8e76b5460101015848c36df1f9f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-19 13:20:36 -0600

    Add sections for each challenge and implementation, add two string methods

---
+++ javascript/learning_3/index.js
@@ -0,0 +1,34 @@
+// String methods
+
+// charAt()
+
+let exampleString = "Hello World";
+console.log(exampleString.charAt(2)); // => "l" third character index 2
+
+// slice()
+
+let exString = "JavaScript";
+console.log(exString.slice(1, 4)); // => output "ava" slice from index 1 - 3, 4 is exclusive
+
+
+// String challenges
+
+// Number methods
+
+// Number Challenges
+
+// Array methods
+
+// Array Challenges
+
+// Object Methods
+
+// Object Challenges
+
+// Loops Methods
+
+// Loops Challenges
+
+// Intermediate Challenges
+
+// Advanced challenges
