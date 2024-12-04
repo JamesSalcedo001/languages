84fb49e46b38cddbd299f31041dc7c2f71e7d030
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-03 23:45:02 -0600

    Create card function

--- javascript/learning_3/frontend_components/index.js
+++ javascript/learning_3/frontend_components/index.js
@@ -44,35 +44,63 @@
 // ex 2 add input field with label
 
 
-function createInputWithLabel(labelText, inputType = "text") {
-    // first create the main container element, label and input elements
-    const container = document.createElement('div');
-    const label = document.createElement("label");
-    const input = document.createElement("input")
+// function createInputWithLabel(labelText, inputType = "text") {
+//     // first create the main container element, label and input elements
+//     const container = document.createElement('div');
+//     const label = document.createElement("label");
+//     const input = document.createElement("input")
 
 
-    // set the text content of the label to the first argument given in the function
-    label.textContent = labelText;
+//     // set the text content of the label to the first argument given in the function
+//     label.textContent = labelText;
 
-    // above, input type default is set to text unless specified in the second argyment passed to the function
+//     // above, input type default is set to text unless specified in the second argyment passed to the function
 
-    // setting the type of the input element
-    input.type = inputType;
+//     // setting the type of the input element
+//     input.type = inputType;
 
 
-    // append label and input as children of the container element
-    container.appendChild(label);
-    container.appendChild(input);
+//     // append label and input as children of the container element
+//     container.appendChild(label);
+//     container.appendChild(input);
 
-    return container;
+//     return container;
 
+// }
+
+
+// // adding the input field
+
+// // invoke function and set to a variable
+// const inputWithLabel = createInputWithLabel("Enter your name: ");
+
+// // append the newly created input as a child of the app div
+// document.getElementById("app").appendChild(inputWithLabel);
+
+
+
+
+// ex 2 add a card
+
+
+function createCard(title, content) {
+    // initialize variable, storing a created div element
+    const card = document.createElement("div");
+
+    // add a className to the card element of 'card'
+    card.className = "card";
+
+    // create new elements for cardTitle and cardContent
+    const cardTitle = document.createElement("h3");
+    const cardContent = document.createElement("p");
+
+
+    // append the newly created h3 and p elements to the card element
+    card.appendChild(cardTitle);
+    card.appendChild(cardContent);
+
+    return card
 }
 
 
-// adding the input field
 
-// invoke function and set to a variable
-const inputWithLabel = createInputWithLabel("Enter your name: ");
-
-// append the newly created input as a child of the app div
-document.getElementById("app").appendChild(inputWithLabel);
