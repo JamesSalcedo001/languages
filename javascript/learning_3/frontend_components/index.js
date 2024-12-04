1cbce4577d665c7dfeae5415a75c4a568ecfb5ee
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-04 00:32:10 -0600

    Add button to close modal, and css for card class

--- javascript/learning_3/frontend_components/index.js
+++ javascript/learning_3/frontend_components/index.js
@@ -80,90 +80,118 @@
 
 
 
-// ex 2 add a card
+// ex 3 add a card
 
 
-// function createCard(title, content) {
-//     // initialize variable, storing a created div element
-//     const card = document.createElement("div");
+function createCard(title, content) {
+    // initialize variable, storing a created div element
+    const card = document.createElement("div");
 
-//     // add a className to the card element of 'card'
-//     card.className = "card";
+    // add a className to the card element of 'card'
+    card.className = "card";
 
-//     // create new elements for cardTitle and cardContent
-//     const cardTitle = document.createElement("h3");
-//     const cardContent = document.createElement("p");
+    // create new elements for cardTitle and cardContent
+    const cardTitle = document.createElement("h3");
+    const cardContent = document.createElement("p");
 
-//     // assign the textcontent of the card's title and content to the arguments passed into the function
-//     cardTitle.textContent = title;
-//     cardContent.textContent = content;
-
-
-//     // append the newly created h3 and p elements to the card element
-//     card.appendChild(cardTitle);
-//     card.appendChild(cardContent);
-
-//     return card
-// }
+    // assign the textcontent of the card's title and content to the arguments passed into the function
+    cardTitle.textContent = title;
+    cardContent.textContent = content;
 
 
-// // adding card
+    // append the newly created h3 and p elements to the card element
+    card.appendChild(cardTitle);
+    card.appendChild(cardContent);
 
-// const card = createCard("Card Title", "This is the card content.");
-
-// // append the card element as a child to app
-// document.getElementById("app").appendChild(card);
-
-
-
-
-
-
-
-
-
-// ex 2 add a modal
-
-
-function createModal(contentText, onClose) {
-    // create container element
-    const modal = document.createElement("div");
-
-    // assign a class of modal and the display property to none
-    modal.className = "modal";
-    modal.style.display = "none";
-
-    // create another div to store the content in the modal
-    const modalContent = document.createElement("div");
-    
-    // aassign this div the class of modal-content
-    modalContent.className = "modal-content";
-
-    // create a paragraph element inside the content div to display words
-    const content = document.createElement("p");
-
-    // assign the textContent of the paragraph to be whatever is passed as the first argument of the createModal function
-    content.textContent = contentText;
-
-
-    // create a button to close the modal
-    const closeButton = document.createElement("button");
-
-    // Set the textContent property to a value of "Close"
-    closeButton.textContent = "Close";
-
-    // attach click event listener to the button which sets the modal display property back to none, which makes it invisible
-    closeButton.addEventListener("click", () => {
-        modal.style.display = "none";
-        if (onClose) onClose();
-    });
-
-    // appending content as a child of modalContent, closeButton as a child of modalContent, and appending modalContent as a child of modal
-    modalContent = appendChild(content);
-    modalContent.appendChild(closeButton);
-    modal.appendChild(modalContent);
-
-    return modal;
+    return card
 }
 
 
+// adding card
+
+const card = createCard("Card Title", "This is the card content.");
+
+// append the card element as a child to app
+document.getElementById("app").appendChild(card);
+
+
+
+
+
+
+
+
+
+// ex 4 add a modal
+
+
+// function createModal(contentText, onClose) {
+//     // create container element
+//     const modal = document.createElement("div");
+
+//     // assign a class of modal and the display property to none
+//     modal.className = "modal";
+//     modal.style.display = "none";
+
+//     // create another div to store the content in the modal
+//     const modalContent = document.createElement("div");
+    
+//     // aassign this div the class of modal-content
+//     modalContent.className = "modal-content";
+
+//     // create a paragraph element inside the content div to display words
+//     const content = document.createElement("p");
+
+//     // assign the textContent of the paragraph to be whatever is passed as the first argument of the createModal function
+//     content.textContent = contentText;
+
+
+//     // create a button to close the modal
+//     const closeButton = document.createElement("button");
+
+//     // Set the textContent property to a value of "Close"
+//     closeButton.textContent = "Close";
+
+//     // attach click event listener to the button which sets the modal display property back to none, which makes it invisible
+//     closeButton.addEventListener("click", () => {
+//         modal.style.display = "none";
+//         if (onClose) onClose();
+//     });
+
+//     // appending content as a child of modalContent, closeButton as a child of modalContent, and appending modalContent as a child of modal
+//     modalContent.appendChild(content);
+//     modalContent.appendChild(closeButton);
+//     modal.appendChild(modalContent);
+
+//     return modal;
+// }
+
+
+
+// function createButton(text, onClick) {
+//     const button = document.createElement("button");
+
+//     button.textContent = text;
+
+//     button.addEventListener("click", onClick);
+
+//     return button;
+// }
+
+
+
+
+// // invoke function to create modal and store it as variable
+
+// const modal = createModal("This is modal content", () => console.log("Modal Closed."));
+
+// // append created modal to the dom
+// document.body.appendChild(modal);
+
+
+// // button to show modal
+// const showModalButton = createButton("Show Modal", () => {
+//     modal.style.display = "flex";
+// });
+
+// document.getElementById("app").appendChild(showModalButton);
