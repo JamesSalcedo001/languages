a487a125ef7dad7af497d557a66f98ee8491fc0e
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-03 23:18:31 -0600

    Create button function

--- javascript/learning_3/frontend_components/index.js
+++ javascript/learning_3/frontend_components/index.js
@@ -1,5 +1,15 @@
 // ex 1 create button
 
-function createButton() {
+function createButton(text, onClick) {
+    // create an element of type button and assign a variable name
+    const button = document.createElement("button");
 
+    // assign the content of the button's text to equal whatever is input as the first argument of the createButton function
+    button.textContent = text;
+
+    // attach a click event listener to the newly created button element
+    button.addEventListener("click", onClick);
+
+    return button;
+    
 }
