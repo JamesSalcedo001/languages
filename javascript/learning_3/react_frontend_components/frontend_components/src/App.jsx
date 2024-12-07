999f82ea80a30cab9417bda66b4ce3b501aa5017
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-06 20:15:09 -0600

    Add Button component, import it into App component

--- javascript/learning_3/react_frontend_components/frontend_components/src/App.jsx
+++ javascript/learning_3/react_frontend_components/frontend_components/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Button } from './components/Button'
 
 function App() {
 
   return (
-    <div>Hello</div>
+    <div style={{ padding: "20px" }}>
+      <h1>React Component Showcase</h1>
+
+      {/* Create Button component, passing string click me as prop named 'text' to the button component, and passing a callback function as a prop named 'onClick'. when button is clicked this function executes and shows alert with message 'Button Clicked!' */}
+
+      <Button text="Click Me" onClick={() => alert("Button Clicked!")} />
+        
+    </div>
   )
 }
 
---
+++ javascript/learning_3/react_frontend_components/frontend_components/src/components/Button.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export const Button = ({ text, onClick }) => {
+    // define functional React component and destructure text and onClick props passed to the button component 
+
+    // attaches onClick prop as click event handler for the button, and displays the value of the 'text' prop as the button's label
+    
+    return <button onClick={onClick}>{text}</button>;
+}
+
+ 
