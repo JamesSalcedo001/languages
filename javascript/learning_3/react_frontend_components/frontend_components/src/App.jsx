32ff4ec5fa6426bdc00e1d49e14c8c74f462dcb6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-06 20:48:10 -0600

    Add Modal component

--- javascript/learning_3/react_frontend_components/frontend_components/src/App.jsx
+++ javascript/learning_3/react_frontend_components/frontend_components/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 // import { Button } from './components/Button'
-import { Card } from './components/Card.'
-
+// import { Card } from './components/Card.'
+import { Modal } from './components/Modal'
 
 function App() {
 
@@ -14,8 +14,12 @@ function App() {
       {/* <Button text="Click Me" onClick={() => alert("Button Clicked!")} /> */}
 
       {/* Create Card component, passing string Card Title and This is card content as props named title and content to the card component */}
-      
-      <Card title="Card Title" content="This is card content." />
+
+      {/* <Card title="Card Title" content="This is card content." /> */}
+
+      {/* Create Modal component, passing string and a callback function as props. when button is clicked, this function executes and shows an alert with message Button Clicked */}
+
+      <Modal content="This is a modal" onClose={() => alert("Modal closed")} />
 
     </div>
   )
---
+++ javascript/learning_3/react_frontend_components/frontend_components/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+export const Modal = ({ content, onClose }) => {
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+                <div style={{ background: "#fff", padding: "20px", borderRadius: "5px" }}>
+                    <p>{content}</p>
+                    <button onClick={onClose}>Close</button>
+                </div>
+            </div>
+    );
+};
