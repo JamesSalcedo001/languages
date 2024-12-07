6731dc20ec291e99652dab39f27aaed65c8a3ba1
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-06 21:17:39 -0600

    Migrate CSS from Modal component to App.css

--- javascript/learning_3/react_frontend_components/frontend_components/src/components/Modal.jsx
+++ javascript/learning_3/react_frontend_components/frontend_components/src/components/Modal.jsx
@@ -1,20 +1,8 @@
 /* eslint-disable react/prop-types */
 export const Modal = ({ content, onClose }) => {
     return (
-        <div
-            style={{
-                position: "fixed",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-            >
-                <div style={{ background: "#fff", padding: "20px", borderRadius: "5px" }}>
+        <div className="modal">
+                <div className="modal-content">
                     <p>{content}</p>
                     <button onClick={onClose}>Close</button>
                 </div>
